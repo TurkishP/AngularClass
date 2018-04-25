8f40c7177bd2df5d46b03c90a1dcbf5f63e4abea
TurkishP <jk_9397@hotmail.com>
2018-04-25 16:29:17 +0900

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+
+const routes: Routes = [
+  
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent }
+  
+];
+
+@NgModule({
+  // The method is called forRoot() because you configure the router 
+  // at the application's root level. The forRoot() method supplies 
+  // the service providers and directives needed for routing, 
+  // and performs the initial navigation based on the current browser URL.
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+  
+})
+
+
+export class AppRoutingModule {}
